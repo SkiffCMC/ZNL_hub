1210ecea50c9bdee8a089de2fed727c67f6c7343
SkiffCMC <skiffcmc@gmail.com>
2019-01-01 17:48:26 +0300

    Fixed a typo

--- src/utils/getNetwork.js
+++ src/utils/getNetwork.js
@@ -2,7 +2,8 @@ import networks from '../constants/networks';
 
 export default (code) => {
   let network;
-  console.log('In getNetwork with code={$code}');
+  console.log('In getNetwork with code:');
+  console.log(code);
   Object.keys(networks).forEach((key) => {
     if (networks[key].code === code) {
       network = networks[key];
