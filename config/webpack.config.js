9fa7b863239b30aafd41b37fc87a661cd458f348
SkiffCMC <skiffcmc@gmail.com>
2019-01-05 15:55:08 +0300

    Fixed a typo

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       {
         enforce: 'pre',
         test: /\.js$/,
-        exclude: node_modules/,
+        exclude: /node_modules/,
         loader: 'eslint-loader',
         options: {},
       },
