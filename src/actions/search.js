b56e8219416f29502c51e0a99bf94b813de87d65
SkiffCMC <skiffcmc@gmail.com>
2019-01-13 14:08:58 +0300

    Added debug output

--- src/actions/search.js
+++ src/actions/search.js
@@ -89,6 +89,8 @@ export const searchTransactions = ({
 }) =>
   (dispatch, getState) => {
     const liskAPIClient = getState().peers.liskAPIClient;
+    console.log('In searchTransactions with APIClient: ');
+    console.log(LiskAPIClient);
     if (showLoading) dispatch(loadingStarted(actionTypes.searchTransactions));
     getTransactions({
       liskAPIClient, address, limit, filter,
